6b18467ab7f10edfd4e2ea409f1f86b909a87911
Obinna Nnenanya <obinnaeye@yahoo.com>
2023-05-13 23:24:58 +0100

    185171924:Add Swagger doc

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import config from "config";
+import swaggerUi from "swagger-ui-express";
 import log from "./logger";
 import connect from "./db/connect";
 import routes from "./routes";
 import deserializeAttendee from "./middleware/deserializeUser";
+import * as swggerDoc from "../swagger.json";
 
-const port = config.get("port") as number;
+const port = Number(process.env.PORT) || config.get("port") as number;
 const host = config.get("host") as string;
 
 const app = express();
@@ -13,6 +15,12 @@ app.use(deserializeAttendee);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(
+  "/docs",
+  swaggerUi.serve,
+  swaggerUi.setup(swggerDoc)
+);
+
 
 app.listen(port, host, () => {
   log.info(`Server listing at http://${host}:${port}`);
