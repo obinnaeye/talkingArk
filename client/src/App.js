fc68d8c113ddd5352eb551fabfc1322db047cede
Obinna Nnenanya <obinnaeye@yahoo.com>
2023-05-14 22:35:37 +0100

    185173879:Create chatroom

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { io } from "socket.io-client";
+import Chat from './pages/chat';
 import Home from './pages/home';
 
 const socket = io(process.env.REACT_APP_SOCKECT_URL, {
@@ -27,6 +28,10 @@ function App() {
                 socket={socket}
               />
             }
+          />
+          <Route
+            path='/chat'
+            element={<Chat username={username} room={room} socket={socket} />}
           />
         </Routes>
       </div>
--- server/src/app.ts
+++ server/src/app.ts
@@ -1,3 +1,4 @@
+import 'dotenv/config'
 import express from "express";
 import config from "config";
 import swaggerUi from "swagger-ui-express";
@@ -8,10 +9,12 @@ import connect from "./db/connect";
 import routes from "./routes";
 import deserializeAttendee from "./middleware/deserializeUser";
 import * as swggerDoc from "../swagger.json";
+import { harperSaveMessage } from './harperSaveMessage';
+import { harperGetMessages } from './harperGetMessage';
 
 const port = Number(process.env.PORT) || config.get("port") as number;
 const host = config.get("host") as string;
-
+console.log(console.log(process.env.HARPERDB_URL))
 const app = express();
 app.use(deserializeAttendee);
 
@@ -32,10 +35,17 @@ const io = new Server(server, {
 });
 
 const CHAT_BOT = 'ChatBot';
+let chatRoom = ''; // E.g. javascript, node,...
+let allUsers: any[] = [];
 io.on('connection', (socket) => {
   console.log(`User connected ${socket.id}`);
   socket.on('join_room', (data) => {
     const { username, room } = data;
+    harperGetMessages(room)!
+      .then((last100Messages) => {
+        socket.emit('last_100_messages', last100Messages);
+      })
+      .catch((err) => console.log(err));
     socket.join(room);
     let __createdtime__ = Date.now();
     socket.to(room).emit('receive_message', {
@@ -48,6 +58,18 @@ io.on('connection', (socket) => {
       username: CHAT_BOT,
       __createdtime__,
     });
+    chatRoom = room;
+    allUsers.push({ id: socket.id, username, room });
+    const chatRoomUsers = allUsers.filter((user) => user.room === room);
+    socket.to(room).emit('chatroom_users', chatRoomUsers);
+    socket.emit('chatroom_users', chatRoomUsers);
+  });
+  socket.on('send_message', (data) => {
+    const { message, username, room, __createdtime__ } = data;
+    io.in(room).emit('receive_message', data);
+    harperSaveMessage(message, username, room, __createdtime__)!
+      .then((response) => console.log(response))
+      .catch((err) => console.log(err));
   });
 });
 
