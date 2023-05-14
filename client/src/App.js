52360c2c0cd338f038b5f762307bb0e5b536b3f5
Obinna Nnenanya <obinnaeye@yahoo.com>
2023-05-14 20:54:14 +0100

    185173749:Connect client to socket

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,33 @@
 import './App.css';
+import { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { io } from "socket.io-client";
 import Home from './pages/home';
 
+const socket = io(process.env.REACT_APP_SOCKECT_URL, {
+  withCredentials: false,
+});
+
 function App() {
+  const [username, setUsername] = useState('');
+  const [room, setRoom] = useState('');
+
   return (
     <Router>
       <div className='App'>
         <Routes>
-          <Route path='/' element={<Home />} />
+          <Route
+            path='/'
+            element={
+              <Home
+                username={username}
+                setUsername={setUsername}
+                room={room}
+                setRoom={setRoom}
+                socket={socket}
+              />
+            }
+          />
         </Routes>
       </div>
     </Router>
